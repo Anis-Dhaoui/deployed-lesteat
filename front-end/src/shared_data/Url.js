d2b12403ba40f5959a58431c482ac85d41f3a13e
Anis Dhaoui <freeconcept6@gmail.com>
2022-12-29 18:38:59 +0100

    Commit before NPM FIX AUDIT

--- front-end/src/shared_data/Url.js
+++
@@ -1,8 +0,0 @@
-// JSON SERVER URL
-// export const url = 'http://localhost:3001/';
-
-// NODEJS SERVER URL
-// export const url = 'https://localhost:3443/';
-
-// HEROKU SERVER URL
-export const url = '/';
