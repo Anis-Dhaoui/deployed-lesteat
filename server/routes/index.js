69109a2f5a85c5d93288a3c7a37e1829b44fdc07
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-07 22:42:55 +0100

    Debugging reload page error

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ var path = require('path');
 router.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   res.sendFile(path.join(__dirname, 'https://letseat-z2qv.onrender.com/index.html'));
+  res.redirect('https://letseat-z2qv.onrender.com/index.html')
 });
 
 module.exports = router;
