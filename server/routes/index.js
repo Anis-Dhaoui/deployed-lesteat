98a737e1db1f9b6f1ccba69772288503cb4dd58b
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-07 22:16:06 +0100

    Fixing NOT FOUND ERROR when reloading page on production (../../client/build)

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('*', (req, res) => {
-  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 module.exports = router;
