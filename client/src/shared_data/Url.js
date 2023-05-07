6138f2f21f3b3c0bd2cba9fcd6958b34a954d819
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-07 20:57:55 +0100

    Add render server

--- client/src/shared_data/Url.js
+++ client/src/shared_data/Url.js
@@ -2,13 +2,10 @@
 // export const url = 'http://localhost:3001/';
 
 // NODEJS SERVER URL
-export const url = 'http://localhost:3000/';
+// export const url = 'http://localhost:3000/';
 
 // HEROKU SERVER URL
 // export const url = '/';
 
 // RENDER SERVER URL
-// export const url = 'https://letseat-api.onrender.com/'
-
-// DOCKER SERVER URL
-// export const url = 'server'
+export const url = 'https://letseat-api.onrender.com/'
