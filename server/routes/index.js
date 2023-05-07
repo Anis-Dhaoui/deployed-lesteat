64a51700c4ff54c2d959b02fd50ed53f246beeae
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-07 22:24:36 +0100

    Reloading page error persist...

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,12 @@ var path = require('path');
 
 /* GET home page. */
 router.get('*', (req, res) => {
+  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+  console.log(req.path)
+  console.log(req.url)
+  console.log(req.baseUrl);
+  console.log(req.params);
+  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
