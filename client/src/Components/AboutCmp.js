8282fe0f549d83c9895440d0e1a32ef66b824de9
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-08 21:38:56 +0100

    About us

--- client/src/Components/AboutCmp.js
+++ client/src/Components/AboutCmp.js
@@ -44,20 +44,20 @@ function About(props) {
                     <h2>About Us</h2>
                     <div className="pb-3">
                         <p>
-                            Since our modest beginnings in 2005 with a little space in Toronto’s stylish Yorkville locale, ‘Organization Name’ ‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food.
+                            Since our modest beginnings in 2005 with a little space in Parlin’s stylish Yorkville locale, LetsEat‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food.
 
-                            In contrast to other Indian eateries, ‘Organization Name’ was made with the explicit expectation to appear as something else.
+                            In contrast to other Indian eateries, LetsEat was made with the explicit expectation to appear as something else.
                         </p>
                         <p>
                             We realize numerous individuals love Indian sustenance, yet a large number of them loathe or are unconscious of the regularly unfortunate fixings that make run-of-the-mill Indian nourishment taste so great.
 
                             Our menu highlights things that utilize the sound and fragrant flavors, however, forget the stuffing ghee, spread, oil, and overwhelming cream.
 
-                            ‘Organization Name’ has developed to incorporate four superb takeout areas in Toronto, with additional to come sooner rather than later. Our group takes pride in the way that we can furnish our new and faithful clients with extraordinary tasting Indian-roused nourishment that is not normal for that some other Indian eateries you visit.
+                            LetsEat has developed to incorporate four superb takeout areas in Toronto, with additional to come sooner rather than later. Our group takes pride in the way that we can furnish our new and faithful clients with extraordinary tasting Indian-roused nourishment that is not normal for that some other Indian eateries you visit.
                         </p>
                         <p>
-                            We perceive that a few people are as yet searching for run-of-the-mill Indian nourishment, and that is fine with us. Our disclaimer is that on the off chance that you’re anticipating overwhelming, slick, undesirable Indian nourishment, ‘Organization Name’ isn’t the place for you.
-                            Our expectation is that you’ll join the developing pattern that such a large number of others have officially found and you will attempt ‘Organization Name’ as a remarkable option to other Indian eateries as well as to all other solid sustenance alternatives out there!
+                            We perceive that a few people are as yet searching for run-of-the-mill Indian nourishment, and that is fine with us. Our disclaimer is that on the off chance that you’re anticipating overwhelming, slick, undesirable Indian nourishment, LetsEat isn’t the place for you.
+                            Our expectation is that you’ll join the developing pattern that such a large number of others have officially found and you will attempt LetsEat as a remarkable option to other Indian eateries as well as to all other solid sustenance alternatives out there!
                         </p>
                     </div>
                 </div>
